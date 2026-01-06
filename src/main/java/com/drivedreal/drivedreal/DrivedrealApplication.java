973eaa92bb3e5cf6aa47827e82a985563129d953
sandjonyves <sandjonyves@gmail.com>
2026-01-06 10:12:35 -0800

    feat. create base structure of project

---
+++ src/main/java/com/drivedreal/drivedreal/DrivedrealApplication.java
@@ -0,0 +1,13 @@
+package com.drivedreal.drivedreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivedrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivedrealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drivedreal/drivedreal/DrivedrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivedreal.drivedreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivedrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
